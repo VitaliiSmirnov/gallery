0419fdac4d6fc542df49ed991f1b446be4e2d2be
= <=>
2018-04-20 16:49:54 +0300

    add index file

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel=”stylesheet” href=”css/style.css” >
+    <title>Пример адаптивного сайта</title>
+
+</head>
+<body>
+    <div>
+        Привет
+    </div>
+</body>
+</html>
