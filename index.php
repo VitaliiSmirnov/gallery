570948cfed00bcdb14974e3b23e018c1d68ee1eb
= <=>
2018-04-27 17:03:55 +0300

    adaptive gallary

--- index.php
+++ index.php
@@ -1,14 +1,36 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <?php
+        define ("title", "Пример адаптивного сайта");
+        $image="https://fakeimg.pl/350x200/?text=World&font=lobster";
+    ?>
     <meta charset="utf-8">
-    <link rel=”stylesheet” href=”css/style.css” >
-    <title>Пример адаптивного сайта</title>
+    <link rel="stylesheet" href="css/style.css">
+    <title><?php echo title; ?></title>
 
 </head>
 <body>
-    <div>
-        Привет
+    <div class="image_gallery">
+        <div>
+            <img src="<?php echo $image ?>">
+        </div>        <div>
+            <img src="<?php echo $image ?>">
+        </div>        <div>
+            <img src="<?php echo $image ?>">
+        </div>        <div>
+            <img src="<?php echo $image ?>">
+        </div>        <div>
+            <img src="<?php echo $image ?>">
+        </div>        <div>
+            <img src="<?php echo $image ?>">
+        </div>        <div>
+            <img src="<?php echo $image ?>">
+        </div>        <div>
+            <img src="<?php echo $image ?>">
+        </div>        <div>
+            <img src="<?php echo $image ?>">
+        </div>
     </div>
 </body>
 </html>
