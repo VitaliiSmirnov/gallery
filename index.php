9aa98d2c335418cce6f9e8fdb07e4bdc8cda9122
= <=>
2018-05-08 17:10:33 +0300

    Added cycle

--- index.php
+++ index.php
@@ -5,19 +5,23 @@
         include "pg/main.php"
     ?>
     <meta charset="utf-8">
+    <title><?php echo TITLE; ?></title>
     <link rel="stylesheet" href="css/style.css">
-    <title><?php echo title; ?></title>
+    <script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.js"></script>
 </head>
 <body>
     <div class="image_gallery">
-        <div>
-            <img src="<?php echo($image[0]['url']); ?>">
-        </div>
-        <div>
-            <img src="<?php echo($image[1]['url']); ?>">
-        </div>
-
+        <?php foreach ($images as $image): ?>
+            <div>
+                <a data-fancybox="gallery"
+                    href="<?php echo isset($image['url']) ? $image['url'] : IMAGE_PLACEHOLDER ?>">
+                    <img src="<?php echo isset($image['thumbnail']) ? $image['thumbnail'] : IMAGE_PLACEHOLDER ?>">
+                </a>
+            </div>
+        <?php endforeach; ?>
     </div>
 </body>
 </html>
